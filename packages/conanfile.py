a90dc1bd62d286bb995024b0739e0cd60d6e4afd
Shiv <42866319+iamshivprasad@users.noreply.github.com>
2023-11-03 23:54:02 +0000

    Added conanfile.py

---
+++ packages/conanfile.py
@@ -0,0 +1,20 @@
+# from conan import ConanFile
+
+# class TicTacToeRecipe(ConanFile):
+    # settings = "os", "compiler", "build_type", "arch"
+    # generators = ""
+    
+    # def requirements(self):
+        # self.requires("sfml/2.6.0")
+        # self.requires("spdlog/1.12.0")
+        
+from conan import ConanFile
+from conan.tools.microsoft import MSBuildDeps
+
+class TicTacToeRecipe(ConanFile):
+    settings = "os", "compiler", "arch", "build_type"
+    requires = "sfml/2.6.0", "spdlog/1.12.0"
+
+    def generate(self):
+        ms = MSBuildDeps(self)
+        ms.generate()
